6de74ece5f742bbee285f02e3d37db28b9b53a51
nahmediz <37185921+nahmediz@users.noreply.github.com>
2023-03-07 19:13:39 +0100

    Feat/001 (#5)
    * story/feat001 : Feature 1 is here

---
+++ src/main/java/org/example/feat/FeatServices.java
@@ -0,0 +1,12 @@
+package org.example.feat;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FeatServices {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(FeatServices.class);
+    public static void main(String[] args) {
+        LOGGER.info("Feature 1 services are here!");
+    }
+}
---
+++ src/main/java/org/feat/FeatureObject.java
@@ -0,0 +1,12 @@
+package org.feat;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FeatureObject {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(FeatureObject.class);
+    public static void main(String[] args) {
+        LOGGER.info("Feature 1 logging");
+    }
+}
